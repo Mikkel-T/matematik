32458dc16a9473cdf76279de0b9a6889a60603d2
Mikkel-T <mikkel@mikkel-t.com>
2021-05-24 20:31:07 +0200

    Add heroicons

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,17 +1,19 @@
+import { HomeIcon } from '@heroicons/react/solid';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 export default function Header() {
   const router = useRouter();
   return (
-    <>
+    <div>
       {router.pathname !== '/' && (
         <Link href="/">
-          <a className="hover:text-nord10 text-nord8 mt-1 ml-2">
-            <span>&#8592;</span> Til startsiden
+          <a className="hover:text-nord10 text-nord8">
+            <HomeIcon className="h-5 w-5 inline-block align-middle mr-1.5" />
+            <span className="inline-block align-middle">Til startsiden</span>
           </a>
         </Link>
       )}
-    </>
+    </div>
   );
 }
