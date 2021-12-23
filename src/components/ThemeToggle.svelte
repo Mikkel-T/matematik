6226e6c12026497f83a0610c3e97f722eb129959
Mikkel-T <mikkel@mikkel-t.com>
2021-12-23 12:35:20 +0100

    Clean up theme toggle classes

--- src/components/ThemeToggle.svelte
+++ src/components/ThemeToggle.svelte
@@ -18,13 +18,14 @@
 
 <div
   on:click={changeTheme}
-  class="inline-flex absolute right-0 items-center m-2 ml-2 w-12 h-6 rounded-full cursor-pointer group bg-nord10"
+  class="inline-flex absolute right-0 items-center m-2 ml-2 w-12 h-6 rounded-full group bg-nord10"
+  class:cursor-pointer={theme}
 >
-  {#if theme}
-    <div
-      class="absolute ml-0.5 w-5 h-5 rounded-full transition-all duration-300 dark:translate-x-6 group-hover:ring-4 bg-nord6 ring-nord11"
-    />
-  {/if}
+  <div
+    class="absolute ml-0.5 w-5 h-5 rounded-full transition-all duration-300 dark:translate-x-6 ring-nord11"
+    class:bg-nord6={theme}
+    class:group-hover:ring-4={theme}
+  />
   <span
     class="absolute w-4 h-4 transition-colors duration-300 translate-x-1 text-nord13 icon dark:text-nord1"
   >
