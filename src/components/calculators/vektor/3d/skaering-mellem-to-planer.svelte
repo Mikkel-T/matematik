bf5669a9e2627a3069b5a2c074d0e38204392c15
Mikkel-T <mikkel@mikkel-t.com>
2024-03-25 15:59:54 +0100

    Another 3d vec calculator

---
+++ src/components/calculators/vektor/3d/skaering-mellem-to-planer.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import Inputs from "@components/calculatorutils/inputs/VectorInputs.svelte";
+  import { Answer, answer, calculator } from "@state/calculator.svelte";
+  import Calculator from "@components/calculatorutils/Calculator.svelte";
+  import { mul, pmat, pmatrix, frac, sub, na } from "@math/calculation";
+
+  function calculate() {
+    let { a_1, b_1, c_1, d_1, a_2, b_2, c_2, d_2 } = calculator.opValues;
+    console.log(calculator.opValues);
+    let p = pmat(
+      na(0),
+      frac(
+        sub(mul(c_1, d_2), mul(c_2, d_1)),
+        sub(mul(b_1, c_2), mul(b_2, c_1)),
+      ),
+      frac(
+        sub(mul(b_2, d_1), mul(b_1, d_2)),
+        sub(mul(b_1, c_2), mul(b_2, c_1)),
+      ),
+    );
+
+    let r = pmat(
+      na(1),
+      frac(
+        sub(mul(a_2, c_1), mul(a_1, c_2)),
+        sub(mul(b_1, c_2), mul(b_2, c_1)),
+      ),
+      frac(
+        sub(mul(b_2, a_1), mul(b_1, a_2)),
+        sub(mul(b_1, c_2), mul(b_2, c_1)),
+      ),
+    );
+    answer.add(
+      new Answer(pmatrix("x", "y", "z"), {
+        answer: `${p.answer} + t \\cdot ${r.answer} \\;,\\; t \\in \\mathbb{R}`,
+        equation: `${p.equation} + t \\cdot ${r.equation} \\;,\\; t \\in \\mathbb{R}`,
+        calculation: `${p.calculation} + t \\cdot ${r.calculation} \\;,\\; t \\in \\mathbb{R}`,
+      }),
+    );
+  }
+</script>
+
+<Calculator {calculate}>
+  <Inputs
+    inputs={[
+      { name: "a_1", type: "number" },
+      { name: "b_1", type: "number" },
+      { name: "c_1", type: "number" },
+      { name: "d_1", type: "number" },
+      { name: "a_2", type: "number" },
+      { name: "b_2", type: "number" },
+      { name: "c_2", type: "number" },
+      { name: "d_2", type: "number" },
+    ]}
+  />
+</Calculator>
